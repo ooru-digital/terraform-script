3828e4d650efac9c319e0a2a3157e748dbf790f4
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-10-30 17:04:23 +0530

    adding opt vm tf code

---
+++ env/prod/compute/opt-vm/output.tf
@@ -0,0 +1,14 @@
+output "instance_ip" {
+  description = "Private IP addresses of the EC2 instances"
+  value       = module.ec2_instance[*].private_ip
+}
+
+output "instance_id" {
+  description = "IDs of the EC2 instances"
+  value       = module.ec2_instance[*].instance_id
+}
+
+output "mgmnt_vm_sg_id" {
+  description = "SG IDs of the EC2 instances"
+  value       = module.opt_vm_sg.sg_id
+}
