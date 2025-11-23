6766f4796ecce46ffa999bb212c561347fe0009b
kpritam8459 <pritam.kondapratiwar@opstree.com>
2025-11-23 15:03:43 +0530

    update: modified backend.tf for stage network skeleton

--- env/stage/network-skeleton/backend.tf
+++ env/stage/network-skeleton/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "credissuer-tf-state"
+    bucket = "credissuer-tf-state-bucket"
     key    = "env/staging/network-skeleton/credissure/terraform.tfstate"
     region = "ap-south-1"
   }
